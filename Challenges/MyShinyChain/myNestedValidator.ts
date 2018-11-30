4839a48b1e6d245b3e1bf0971b68236dedf1bf79
Joshua Biddick <joshua.biddick@cucumber.co.nz>
2018-11-30 13:27:59 +1300

    move solution into solutions folder

--- Challenges/MyShinyChain/myNestedValidator.ts
+++ Challenges/MyShinyChain/myNestedValidator.ts
@@ -1,85 +1,80 @@
 /*
- * Our PM ask us to create a function for password validation
- * Some of the validation rules are the following:
- * - Password cannot be less than 8 characters
- * - Password cannot be greater than 20 characters
- * - Password must have, at least, one capital letter
- * - Password must have, at least, one number
- * - Password must have, at least, one special character from this list !@#$%^&*
- *
- * The following is our first solution
- */
+* Our PM ask us to create a function for password validation
+* Some of the validation rules are the following:
+* - Password cannot be less than 8 characters
+* - Password cannot be greater than 20 characters
+* - Password must have, at least, one capital letter
+* - Password must have, at least, one number
+* - Password must have, at least, one special character from this list !@#$%^&*
+*
+* The following is our first solution
+*/
 
-interface Validator {
-  name: string;
-  validation: (value: any) => boolean;
-}
+let isPasswordValid = function(password: string): Boolean {
+  if (password.length >= 8 && password.length <= 20) {
+      
+      //Checking for capital letter
+      var oneCapital = false;
+      for (var i = 0; i < password.length; i++) {
+          var letter = password[i];
+          if (letter.toUpperCase() == letter) {
+              oneCapital = true;
+              break;
+          }
+      }
 
-// Validation Functions
-function minLengthValidator(value: string, minLength: number): boolean {
-  return value && value.length > minLength;
-}
+      if (oneCapital == false) {
+          return false;
+      }
 
-function maxLengthValidator(value: string, maxLength: number): boolean {
-  return value && value.length < maxLength;
-}
+      //Checking for numbers
+      var oneNumber = false;
+      for (var i = 0; i < password.length; i++) {
+          var letter = password[i];
+          if (typeof letter == "string") {
+              oneNumber = true;
+              break;
+          }
+      }
 
-function containsCapitalLetterValidator(value: string): boolean {
-  const capitalLetterRegex = /[A-Z]/;
-  return value && capitalLetterRegex.test(value);
-}
+      if (oneNumber == false) {
+          return false;
+      }
 
-function containsNumberValidator(value: string): boolean {
-  const capitalLetterRegex = /[0-9]/;
-  return value && capitalLetterRegex.test(value);
-}
+      //
+      var oneSpecialCharacter = false;
+      var specialCharacters = "!@#$%^&*";
+      for (var i = 0; i < password.length; i++) {
+          var letter = password[i];
+          for (var j = 0; j < specialCharacters.length; j++) {
+              var specialCharacter = specialCharacters[j];
+              if (letter == specialCharacter) {
+                  oneSpecialCharacter = true;
+                  break;
+              }
+          }
 
-function containsSpecialCharsValidator(value: string): boolean {
-  const specialCharsRegex = /[!@#$%^&*(),.?":{}|<>]/g;
-  return value && specialCharsRegex.test(value);
-}
+          if (oneSpecialCharacter == true) {
+              break;
+          }
+      }
 
-function isPasswordValid(password: string, validators: Validator[]): boolean {
-  let isValid = validators.every(validator => validator.validation(password));
-  return isValid;
-}
+      if (oneSpecialCharacter == false) {
+          return false;
+      }
 
-// Constants
-const MIN_LENGTH = 8;
-const MAX_LENGTH = 20;
-
-// Validators
-const passwordValidators: Validator[] = [
-  {
-    name: "min-length",
-    validation: value => minLengthValidator(value, MIN_LENGTH)
-  },
-  {
-    name: "max-length",
-    validation: value => maxLengthValidator(value, MAX_LENGTH)
-  },
-  {
-    name: "contains-capital",
-    validation: containsCapitalLetterValidator
-  },
-  {
-    name: "contains-number",
-    validation: containsSpecialCharsValidator
-  },
-  {
-    name: "contains-special-chars",
-    validation: containsCapitalLetterValidator
+      return true;
   }
-];
 
-isPasswordValid("P4ssw0rd$~!", passwordValidators);
+  return false;
+}
 
 /*
- * The PM now is asking us to reuse the password validator function
- * in other part of the solution, but without applying max length restriction
- * additionally, we will need to include another rule that must apply only
- * for this call, but that functionality will be provided later.
- *
- * HINT: There is a design pattern called "chain of responsability" that could
- * fit perfectly for this case. Try to apply it.
- */
+* The PM now is asking us to reuse the password validator function
+* in other part of the solution, but without applying max length restriction
+* additionally, we will need to include another rule that must apply only 
+* for this call, but that functionality will be provided later.
+* 
+* HINT: There is a design pattern called "chain of responsability" that could
+* fit perfectly for this case. Try to apply it.
+*/
