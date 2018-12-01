df3ad2e3205c7ad77fd329c4d62e65c7c06e6230
sadsa <josh.biddick@gmail.com>
2018-12-02 10:30:34 +1300

    made generic pattern validator that accepts a regex

--- Challenges/MyShinyChain/Solutions/JoshBiddick.ts
+++ Challenges/MyShinyChain/Solutions/JoshBiddick.ts
@@ -11,69 +11,59 @@
  */
 
 interface Validator {
-    name: string;
-    validation: (value: any) => boolean;
+  name: string;
+  validation: (value: any) => boolean;
+}
+
+const MIN_LENGTH = 8;
+const MAX_LENGTH = 20;
+const CAPITAL_LETTER_REGEX = /[A-Z]/;
+const CONTAINS_NUMBER_REGEX = /[0-9]/;
+const SPECIAL_CHAR_REGEX = /[!@#$%^&*(),.?":{}|<>]/g;
+
+const passwordValidators: Validator[] = [
+  {
+    name: "min-length",
+    validation: value => minLengthValidator(value, MIN_LENGTH)
+  },
+  {
+    name: "max-length",
+    validation: value => maxLengthValidator(value, MAX_LENGTH)
+  },
+  {
+    name: "contains-capital",
+    validation: value => patternValidator(value, CAPITAL_LETTER_REGEX)
+  },
+  {
+    name: "contains-number",
+    validation: value => patternValidator(value, CONTAINS_NUMBER_REGEX)
+  },
+  {
+    name: "contains-special-chars",
+    validation: value => patternValidator(value, SPECIAL_CHAR_REGEX)
   }
-  
-  // Validation Functions
-  function minLengthValidator(value: string, minLength: number): boolean {
-    return value && value.length > minLength;
-  }
-  
-  function maxLengthValidator(value: string, maxLength: number): boolean {
-    return value && value.length < maxLength;
-  }
-  
-  function containsCapitalLetterValidator(value: string): boolean {
-    const capitalLetterRegex = /[A-Z]/;
-    return value && capitalLetterRegex.test(value);
-  }
-  
-  function containsNumberValidator(value: string): boolean {
-    const capitalLetterRegex = /[0-9]/;
-    return value && capitalLetterRegex.test(value);
-  }
-  
-  function containsSpecialCharsValidator(value: string): boolean {
-    const specialCharsRegex = /[!@#$%^&*(),.?":{}|<>]/g;
-    return value && specialCharsRegex.test(value);
-  }
-  
-  function isPasswordValid(password: string, validators: Validator[]): boolean {
-    let isValid = validators.every(validator => validator.validation(password));
-    return isValid;
-  }
-  
-  // Constants
-  const MIN_LENGTH = 8;
-  const MAX_LENGTH = 20;
-  
-  // Validators
-  const passwordValidators: Validator[] = [
-    {
-      name: "min-length",
-      validation: value => minLengthValidator(value, MIN_LENGTH)
-    },
-    {
-      name: "max-length",
-      validation: value => maxLengthValidator(value, MAX_LENGTH)
-    },
-    {
-      name: "contains-capital",
-      validation: containsCapitalLetterValidator
-    },
-    {
-      name: "contains-number",
-      validation: containsSpecialCharsValidator
-    },
-    {
-      name: "contains-special-chars",
-      validation: containsCapitalLetterValidator
-    }
-  ];
-  
-  isPasswordValid("P4ssw0rd$~!", passwordValidators);
-  
+];
+
+// Validation
+function minLengthValidator(value: string, minLength: number): boolean {
+  return value && value.length > minLength;
+}
+
+function maxLengthValidator(value: string, maxLength: number): boolean {
+  return value && value.length < maxLength;
+}
+
+function patternValidator(value: string, regex: RegExp): boolean {
+  return value && regex.test(value);
+}
+
+function isPasswordValid(password: string, validators: Validator[]): boolean {
+  let isValid = validators.every(validator => validator.validation(password));
+  return isValid;
+}
+
+isPasswordValid("P4ssw0rd$~!", passwordValidators);
+
   /*
    * The PM now is asking us to reuse the password validator function
    * in other part of the solution, but without applying max length restriction
@@ -83,4 +73,3 @@ interface Validator {
    * HINT: There is a design pattern called "chain of responsability" that could
    * fit perfectly for this case. Try to apply it.
    */
-  
